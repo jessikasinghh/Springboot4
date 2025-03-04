5181b6e72e06bdbc65ff13fc7af6723eefb5b475
jessikasinghh <128406656+jessikasinghh@users.noreply.github.com>
2025-03-04 14:18:07 +0530

    UC3-Ability for the Greeting App to give Greeting message with 1. User First Name and Last Name or 2. With just First Name or Last Name based on User attributes provides or 3. Just Hello World.

--- src/main/java/com/example/springboot4/Controller/GreetingController.java
+++ src/main/java/com/example/springboot4/Controller/GreetingController.java
@@ -1,9 +1,9 @@
 package com.example.springboot4.Controller;
 
 import com.example.springboot4.Entity.Greeting;
-import org.springframework.web.bind.annotation.*;
+import com.example.springboot4.Entity.User;
 import com.example.springboot4.Service.GreetingService;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greetings")
@@ -15,32 +15,55 @@ public class GreetingController {
         this.greetingService = greetingService;
     }
 
-    @GetMapping("/hello")
-    public Greeting getSimpleGreeting() {
-        return greetingService.getSimpleGreeting();
-    }
-
-    @GetMapping("/{name}")
-    public Greeting getGreeting(@PathVariable String name) {
-        return greetingService.getPersonalizedGreeting(name);
-    }
-
     @PostMapping
-    public Greeting createGreeting(@RequestBody String name) {
-        return greetingService.createGreeting(name);
-    }
-
-    @PutMapping("/{name}")
-    public Greeting updateGreeting(@PathVariable String name) {
-        return greetingService.updateGreeting(name);
-    }
-
-    @DeleteMapping("/{name}")
-    public Greeting deleteGreeting(@PathVariable String name) {
-        return greetingService.deleteGreeting(name);
+    public Greeting getGreeting(@RequestBody User user) {
+        return greetingService.getGreeting(user);
     }
 }
 
+
+
+//import com.example.springboot4.Entity.Greeting;
+//import org.springframework.web.bind.annotation.*;
+//import com.example.springboot4.Service.GreetingService;
+//
+//
+//@RestController
+//@RequestMapping("/greetings")
+//public class GreetingController {
+//
+//    private final GreetingService greetingService;
+//
+//    public GreetingController(GreetingService greetingService) {
+//        this.greetingService = greetingService;
+//    }
+//
+//    @GetMapping("/hello")
+//    public Greeting getSimpleGreeting() {
+//        return greetingService.getSimpleGreeting();
+//    }
+//
+//    @GetMapping("/{name}")
+//    public Greeting getGreeting(@PathVariable String name) {
+//        return greetingService.getPersonalizedGreeting(name);
+//    }
+//
+//    @PostMapping
+//    public Greeting createGreeting(@RequestBody String name) {
+//        return greetingService.createGreeting(name);
+//    }
+//
+//    @PutMapping("/{name}")
+//    public Greeting updateGreeting(@PathVariable String name) {
+//        return greetingService.updateGreeting(name);
+//    }
+//
+//    @DeleteMapping("/{name}")
+//    public Greeting deleteGreeting(@PathVariable String name) {
+//        return greetingService.deleteGreeting(name);
+//    }
+//}
+//
 
 
 //@RestController
--- src/main/java/com/example/springboot4/Service/GreetingService.java
+++ src/main/java/com/example/springboot4/Service/GreetingService.java
@@ -1,30 +1,84 @@
 package com.example.springboot4.Service;
 
+//UC3
 import com.example.springboot4.Entity.Greeting;
+import com.example.springboot4.Entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
-    public Greeting getSimpleGreeting() {
-        return new Greeting("Hello World");
-    }
-
-    public Greeting getPersonalizedGreeting(String name) {
-        return new Greeting("Hello, " + name + "!");
-    }
-
-    public Greeting createGreeting(String name) {
-        return new Greeting("Greeting created for " + name);
-    }
-
-    public Greeting updateGreeting(String name) {
-        return new Greeting("Greeting updated for " + name);
-    }
-
-    public Greeting deleteGreeting(String name) {
-        return new Greeting("Greeting deleted for " + name);
+    public Greeting getGreeting(User user) {
+        if (user.getFirstName() != null && user.getLastName() != null) {
+            return new Greeting("Hello, " + user.getFirstName() + " " + user.getLastName() + "!");
+        } else if (user.getFirstName() != null) {
+            return new Greeting("Hello, " + user.getFirstName() + "!");
+        } else if (user.getLastName() != null) {
+            return new Greeting("Hello, " + user.getLastName() + "!");
+        } else {
+            return new Greeting("Hello World!");
+        }
     }
 }
 
 
+
+
+//UC2
+//import com.example.Greeting.model.Greeting;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class GreetingService {
+//
+//    public Greeting getSimpleGreeting() {
+//        return new Greeting("Hello World");
+//    }
+//
+//    public Greeting getPersonalizedGreeting(String name) {
+//        return new Greeting("Hello, " + name + "!");
+//    }
+//
+//    public Greeting createGreeting(String name) {
+//        return new Greeting("Greeting created for " + name);
+//    }
+//
+//    public Greeting updateGreeting(String name) {
+//        return new Greeting("Greeting updated for " + name);
+//    }
+//
+//    public Greeting deleteGreeting(String name) {
+//        return new Greeting("Greeting deleted for " + name);
+//    }
+//}
+
+
+
+//import com.example.springboot4.Entity.Greeting;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class GreetingService {
+//
+//    public Greeting getSimpleGreeting() {
+//        return new Greeting("Hello World");
+//    }
+//
+//    public Greeting getPersonalizedGreeting(String name) {
+//        return new Greeting("Hello, " + name + "!");
+//    }
+//
+//    public Greeting createGreeting(String name) {
+//        return new Greeting("Greeting created for " + name);
+//    }
+//
+//    public Greeting updateGreeting(String name) {
+//        return new Greeting("Greeting updated for " + name);
+//    }
+//
+//    public Greeting deleteGreeting(String name) {
+//        return new Greeting("Greeting deleted for " + name);
+//    }
+//}
+//
+//
